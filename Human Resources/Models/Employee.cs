d7aca67930551571adecb506beb95013208fb8e0
RaminIskhanov <raminii@code.edu.az>
2021-06-23 15:36:17 +0400

    Add project files.

---
+++ Human Resources/Models/Employee.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Human_Resources.Models
+{
+    class Employee
+    {
+        public int No;
+        public static int Count { get; set; } = 1000;
+
+        public string FullName;
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string Position { get; set; }
+
+        public string DepartmentName { get; set; }
+
+        public double Salary { get; set; }
+
+        public Employee()
+        {
+            Count++;
+            No = Count;
+        }
+
+        public Employee(string name, string surname, string position, double salary, string departmentname ):this()
+        {
+            Name = name;
+            SurName = surname;
+
+            Position = position;
+            Salary = salary;
+            DepartmentName = departmentname;
+
+            FullName = Name + " " + SurName;
+
+            //FullName-de Name ve Surname bolmesini 
+
+        }
+        public override string ToString()
+        {
+            return $"{No} {FullName} {Position} {Salary} {DepartmentName}";
+        }
+
+    }
+}
---
+++ Human Resources/Program.cs
@@ -0,0 +1,22 @@
+﻿using Human_Resources.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Human_Resources
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee("Ramin", "Iskhanov", "DishHekimi", 15000, "Hekim");
+            
+            Employee employee2 = new Employee("Pervin", "Iskhanov", "DishHekimi", 15000, "Hekim");
+            Console.WriteLine(employee1.ToString());
+            Console.WriteLine(employee2.ToString());
+
+        }
+    }
+}
